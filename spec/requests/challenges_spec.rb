c99ebaece4475d4496f7520bfeb5ab9b83b3e6b7
Vlad Stoica <vlad96stoica@gmail.com>
2018-04-05 14:11:05 +0300

    travis

--- spec/requests/challenges_spec.rb
+++ spec/requests/challenges_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ChallengesController, type: :request do
-
-
   context '#show' do
-    context "requesting a challenge that doesn't exit" do
-      it 'returns a 404' do
-        get '/challenges/ab'
-        expect(response.status).to eq(404)
+    context "when logged in" do
+      before do
+        login_as(create(:user, role: "admin"), :scope => :user)
+      end
+
+      context "requesting a challenge that doesn't exit" do
+        it 'returns a 404' do
+          get '/challenges/ab'
+
+          expect(response.status).to eq(404)
+        end
       end
     end
   end
