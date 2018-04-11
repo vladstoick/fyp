e43cd947117a03b62ec8ec5ac4c41682d8545f91
Vlad Stoica <vlad96stoica@gmail.com>
2018-04-11 18:32:10 +0300

    Req 4 and 5 spec

--- spec/features/create_a_challenge_spec.rb
+++ spec/features/create_a_challenge_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
-RSpec.feature "Widget management", :type => :feature do
+RSpec.feature "Requirement 4 and 5: Challenge creation", :type => :feature do
   before do
     login_as(create(:user, role: "admin"), :scope => :user)
   end
 
-  context "with no errors" do
+  context "Requirements 4: with no errors" do
     it "creates the challenge" do
       visit "/challenges/new"
 
@@ -17,11 +17,29 @@ RSpec.feature "Widget management", :type => :feature do
 
       click_button "Create"
 
+      expect(page.current_path).to eq("/challenges/#{Challenge.last.id}")
       expect(page).to have_text("Successfully created")
 
       expect(page).to have_text("CREATE TABLE t1(id integer);")
       expect(page).to have_text("INSERT INTO t1(id) VALUES (1), (2);")
       expect(page).to have_text("SELECT * FROM t1")
+    end
+  end
+
+  context "Requirement 5: with errors" do
+    it "creates the challenge" do
+      visit "/challenges/new"
+
+      fill_in "Title",	with: "Challenge 1"
+      fill_in "Content",	with: "Challenge 1 content"
+      fill_in "Sql schema",	with: "CREATE TABLE t1(id integer);"
+      fill_in "Sql seed",	with: "INSERT INTO t1(id) VALUES (1), (2);"
+      fill_in "Sql correct query",	with: "SELECT * FROM t2"
+
+      click_button "Create"
+
+      expect(page.current_path).to eq("/challenges")
+      expect(page).to have_text(".t2' doesn't exist")
     end
   end
 end
